2efe768d7e4d3c7dab9f15f29554938a149c780a
James Newman <jamesmnewman1@gmail.com>
2015-11-22 17:34:58 -0800

    server constant from the api

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -15,7 +15,8 @@ angular
     'ngResource',
     'ngRoute',
     'ngSanitize',
-    'ngTouch'
+    'ngTouch', 
+    'Devise'
   ])
   .config(function ($routeProvider) {
     $routeProvider
@@ -27,11 +28,27 @@ angular
         templateUrl: 'views/about.html',
         controller: 'AboutCtrl'
       })
-      .when('/sign-up', {
-        templateUrl: 'views/signup.html',
+      .when('/register', {
+        templateUrl: 'views/auth/register.html',
         controller: 'AuthCtrl'
+      })
+      .when('/login', {
+        templateUrl: 'views/auth/login.html',
+        controller: 'AuthCtrl'
+      })
+      .when('/products', {
+        templateUrl: 'views/products/index.html',
+        controller: 'ProductsCtrl'
       })
       .otherwise({
         redirectTo: '/'
       });
+  });
+
+  .constant('SERVER', {
+    // Local server
+    //url: 'http://localhost:3000'
+
+    // Public Heroku server
+    url: 'https://api.damp-atoll-6985.herokuapp.com'
   });
--- app/scripts/controllers/authCtrl.js
+++ app/scripts/controllers/authCtrl.js
@@ -1,6 +1,24 @@
 'use strict';
 
 angular.module('mpfeApp')
-.controller('AuthCtrl', function() {
-	
-});
+.controller('AuthCtrl', [
+	'$scope',
+	'$log',
+	'$location',
+	'Auth',
+	// register scope functions to signup or login
+	function($scope, $location, $log, Auth) {
+	  $scope.register = function() {
+		Auth.register($scope.user).then(function(user) {
+		  $log(user);	
+		  $location.path('/products');
+		 });
+	  };
+	  $scope.login = function() {
+		Auth.login($scope.user).then(function(user) {
+		  $log(user);	
+		  $location.path('/products');
+		});
+	   };		
+	}
+]);
